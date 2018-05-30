820ae2de5b492e6b6d7b524aad96a6a6e82a6020
devMozo <devnicolasmozo@gmail.com>
2018-05-30 09:05:59 -0300

    Add testing to the Main Class

--- API/src/test/java/Bootcamp2018/API/ApiApplicationTests.java
+++ API/src/test/java/Bootcamp2018/API/ApiApplicationTests.java
@@ -10,7 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApiApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void applicationContextLoaded() {
+	}
+
+	@Test
+	public void applicationContextTest() {
+		App.main(new String[] {});
 	}
 
 }
