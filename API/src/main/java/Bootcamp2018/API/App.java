f729964d4a23f956e4213eb2ecf7a6435cfeac3d
devMozo <devnicolasmozo@gmail.com>
2018-05-30 11:16:44 -0300

    no message

--- API/src/main/java/Bootcamp2018/API/App.java
+++ API/src/main/java/Bootcamp2018/API/App.java
@@ -1,7 +1,6 @@
 package Bootcamp2018.API;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Represents the app of the project
@@ -13,9 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 	/**
 	 * Main
+	 *
 	 * @param args sdf asdf
 	 */
-    public static void main( String[] args ) {
+	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-    }
+	}
 }
